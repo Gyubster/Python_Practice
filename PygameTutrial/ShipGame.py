810783a43d864a77354fb88a129ce587b9a3a3f8
GYU-BO David SHIM <59437638+Gyubster@users.noreply.github.com>
2020-11-12 02:14:55 +0900

    Boundary added.

--- PygameTutrial/ShipGame.py
+++ PygameTutrial/ShipGame.py
@@ -15,37 +15,57 @@ clock = pygame.time.Clock()
 crashed = False
 shipImg = pygame.image.load('ship.png')
 
+ship_speed = 0
+ship_width, ship_height = 177, 87
+
 def ship(x,y):
     game_display.blit(shipImg, (x, y))
 
-x = (display_width*0.4)
-y = (display_height*0.8)
-x_change = 0
-ship_speed = 0
+def game_loop():
+    game_exit = False
 
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+    x = (display_width*0.4)
+    y = (display_height*0.8)
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
+    x_change = 0
+    y_change = 0
 
-# key pressed로도 구현 가능
+    while not game_exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_exit = True
 
-    x += x_change
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+                elif event.key == pygame.K_UP:
+                    y_change = -5
+                elif event.key == pygame.K_DOWN:
+                    y_change = 5
 
-    game_display.fill(white)
-    ship(x, y)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_change = 0
 
-    pygame.display.update()
-    clock.tick(60)
+        x += x_change
+        y += y_change
 
+        game_display.fill(white)
+        ship(x, y)
+
+        if x < 0 or x > display_width - ship_width:
+            game_exit = True
+
+        if y < 0 or y > display_height - ship_height:
+            game_exit = True
+
+        pygame.display.update()
+        clock.tick(60)
+
+game_loop()
 pygame.quit()
 quit()
