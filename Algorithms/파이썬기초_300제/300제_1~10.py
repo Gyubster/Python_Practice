10eedc68e674c3f081c6021b1f42191fe3f7a0df
GYU-BO David SHIM <59437638+Gyubster@users.noreply.github.com>
2020-12-27 20:31:36 +0900

    Create 300제_1~10.py

---
+++ Algorithms/파이썬기초_300제/300제_1~10.py
@@ -0,0 +1,31 @@
+# 1번
+print('Hello Wolrd')
+
+# 2번
+print("Mary's cosmetics")
+
+# 3번
+print('신씨가 소리질렀다. "도둑이야"')
+
+# 4번
+print("C:\Windows")
+
+# 5번 - \n은 엔터, \t는 탭
+print("안녕하세요.\n만나서\n\t\t반갑습니다.")
+
+# 6번
+print("오늘은", "일요일")
+
+# 7번 - sep은 프린트할 문자열 간의 공간에 원하는것 추가.
+print("naver;kakao;sk;samsung")
+print("naver", "kakao", "sk", "samsung", sep=';')
+
+# 8번
+print("naver", "kakao", "sk", "samsung", sep='/')
+
+# 9번
+print("first", end='')
+print("second")
+
+# 10번
+print(5/3)
