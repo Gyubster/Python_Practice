998ca8956895fb976dc2731504ec45c7b7c284c7
GYU-BO David SHIM <59437638+Gyubster@users.noreply.github.com>
2020-12-28 20:19:04 +0900

    Update 정수제곱근판별.py

--- Algorithms/programmers_level1/정수제곱근판별.py
+++ Algorithms/programmers_level1/정수제곱근판별.py
@@ -1,5 +1,11 @@
+import math
+
 def solution(n):
-    n=0
-    while n<50000000000000:
-    answer = 0
+    number_range = math.sqrt(50000000000000)
+    square_list = [i**2 for i in range(number_range)]
+    if n not in square_list:
+        answer = -1
+    elif n in square_list:
+        number = int(math.sqrt(n))
+        answer = (number+1)**2
     return answer
